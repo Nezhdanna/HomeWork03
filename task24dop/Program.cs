743c7e0a07ba6940401211e0998d975fcebade8b
Nezhdanna <kno17@yandex.ru>
2022-08-13 01:08:38 +0700

    add task24

---
+++ task24dop/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу для разворота массива
+//                  0   1   2   3   4   5   6   7   8   9
+// int[] array = { 56, 45, 13, 98, 67, 83, 25, 17, 40};
+
+using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = {56, 45, 13, 98, 67, 83, 25, 17, 40 };
+        Array.Reverse(array);
+ 
+        Console.WriteLine(String.Join(',', array));
+    }
+}
